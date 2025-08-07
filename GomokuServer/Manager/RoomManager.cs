6bd25b859477cdb512531ff58b3fdad9eadb26d1
jaeeun <jaeeun.lee.4h@gmail.com>
2025-08-07 13:26:18 +0900

    Session을 SuperSocket에서 관리하므로 Room 풀링이 필요 없음

--- GomokuServer/Manager/RoomManager.cs
+++ GomokuServer/Manager/RoomManager.cs
@@ -4,7 +4,6 @@ public class RoomManager
 {
     private Dictionary<int, Room> Rooms = new Dictionary<int, Room>();
     private Dictionary<string, int> SessionIdRoomId = new Dictionary<string, int>(); // session id 로 Room 을 빠르게 찾기 위해 저장
-    private Pool<Room> RoomPool = new Pool<Room>();
     private Room? PendingRoom = null;
 
     public Room Enter(User user)
@@ -16,18 +15,18 @@ public class RoomManager
         }
         else
         {
-            var hasPendingRoom = PendingRoom is not null;
-            var room = hasPendingRoom ? PendingRoom : RoomPool.RentFromPool();
-
-            if (hasPendingRoom)
+            Room room;
+            if (PendingRoom is null)
             {
-                PendingRoom = null; // 2명이 채워졌으니, PendingRoom을 비워줌
+                room = new Room();
+                PendingRoom = room; // 내가 첫 입장이니, 다음 입장할 유저를 위해 PendingRoom에 저장
             }
             else
             {
-                PendingRoom = room; // 내가 첫 입장이니, 다음 입장할 유저를 위해 PendingRoom에 저장
+                room = PendingRoom;
+                PendingRoom = null;
             }
-            
+
             // dic 부터 저장해야, 멀티스레드 환경에서 안전하게 동작함
             Rooms[room.RoomId] = room;
             SessionIdRoomId[user.SessionId] = room.RoomId;
@@ -43,52 +42,42 @@ public class RoomManager
         if (!SessionIdRoomId.TryGetValue(user.SessionId, out int roomId))
             throw new ServerException(ERROR_CODE.UNKNOWN_USER);
 
-        if (!Rooms.TryGetValue(roomId, out Room room))
+        if (!Rooms.TryGetValue(roomId, out var room))
             throw new Exception("impossible fatal error: room not found");
 
         SessionIdRoomId.Remove(user.SessionId); // dic 부터 저장해야, 멀티스레드 환경에서 안전하게 동작함
 
         room.Leave(user);
-        if (room.RoomState == RoomState.Deactivated)
-        {
+        if (room.GetUserCount() <= 0)
             Rooms.Remove(roomId);
-            RoomPool.ReturnToPool(room);
-        }
     }
 }
 
 public class Room
 {
+    // Room
     private static int RoomIdx = 0;
     public int RoomId { get; private set; }
     public RoomState RoomState { get; private set; }
     private Dictionary<string, User> ConnectedUsers = new Dictionary<string, User>();
 
-    public Room()
+    public void Dispose()
     {
-        RoomId = ++RoomIdx;
+        ConnectedUsers.Clear();
     }
 
     public void Enter(User user)
     {
-        // 활성화
-        if (RoomState == RoomState.Deactivated)
-            RoomState = RoomState.Waiting;
-
         ConnectedUsers.Add(user.SessionId, user);
     }
 
     public void Leave(User user)
     {
         ConnectedUsers.Remove(user.SessionId);
-
-        // 비활성화
-        if (ConnectedUsers.Count == 0)
-            RoomState = RoomState.Deactivated;
     }
 
     public void Start()
-    { 
+    {
         RoomState = RoomState.Playing;
     }
 
@@ -106,11 +95,15 @@ public class Room
     {
         return ConnectedUsers.Values.ToList();
     }
+
+    public int GetUserCount()
+    {
+        return ConnectedUsers.Count;
+    }
 }
 
 public enum RoomState
 {
-    Deactivated,
     Waiting,
     Playing
 }
