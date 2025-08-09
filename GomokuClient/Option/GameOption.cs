a2bc6d331626cd47eb58aa06e0ea45c2ab3e89ae
JaeeunDesktop <jaeeun@com2us.com>
2025-08-09 10:39:39 +0900

    오목판 ui 추가

---
+++ GomokuClient/Option/GameOption.cs
@@ -0,0 +1,6 @@
+﻿public static class GameOption
+{
+    public static int MaxUserCountPerServer => MaxRoomCountPerServer * 2; // 서버당 최대 유저 수 (오목 서버이니 방당 2명)
+    public static int MaxRoomCountPerServer = 10; // 서버당 최대 방 개수 // TODO 적용
+    public static int BoardSize = 15; // 바둑판 크기
+}
